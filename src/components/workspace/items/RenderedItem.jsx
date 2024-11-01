fd47f6d9188eb2fac0bc6f27ab178bbedc746bd1
veta <vee.marh.my@gmail.com>
2024-11-01 23:12:11 +0300

    fixed html sanitize

--- src/components/workspace/items/RenderedItem.jsx
+++ src/components/workspace/items/RenderedItem.jsx
@@ -2,41 +2,21 @@ import styles from '../../../assets/css/Contents.module.css';
 import {HeaderItem, SubHeaderItem, SubSubHeaderItem, TextItem} from './Items.jsx';
 import {useCallback} from 'react';
 import sanitizeHtml from 'sanitize-html';
+import {isCaretAtEnd, getSelectionEnd, getSelectionStart, setSelectionRange} from '../selection/Selection.js';
 
 export default function RenderedItem({id, type, content, setContent, onDelete, onEnter}) {
     const onContentChange = useCallback(evt => {
+        const prevSelection = {start: getSelectionStart(evt.currentTarget), end: getSelectionEnd(evt.currentTarget)};
         const sanitizeConf = {
-            allowedTags: ["b", "i", "u", "s", "del", "a", "p", "ul", "ol", "li", "h2", "h3", "h4"],
+            allowedTags: ["b", "i", "em", "strong", "u", "s", "a", "p", "ul", "ol", "li"],
             allowedAttributes: {a: ["href"]}
         };
 
-        setContent(sanitizeHtml(evt.currentTarget.innerHTML, sanitizeConf))
-    }, [setContent])
-
-    function getTextLength(elem) {
-        let range = elem.ownerDocument.createRange();
-        range.selectNodeContents(elem);
-
-        return range.toString().length;
-    }
-
-    function getCaretOffset(elem) {
-        let sel = elem.ownerDocument.defaultView.getSelection();
-        if (sel.rangeCount === 0) return 0;
-
-        let range = elem.ownerDocument.defaultView.getSelection().getRangeAt(0);
-        let preCaretRange = range.cloneRange();
-        preCaretRange.selectNodeContents(elem);
-        preCaretRange.setEnd(range.endContainer, range.endOffset);
-        return preCaretRange.toString().length;
-    }
-
-    function isCaretAtEnd(elem) {
-        if (elem.ownerDocument.activeElement !== elem) return false;
-        if (elem.ownerDocument.defaultView.getSelection().getRangeAt(0).toString().length > 0) return false;
-
-        return getCaretOffset(elem) === getTextLength(elem);
-    }
+        setContent(sanitizeHtml(evt.currentTarget.innerHTML, sanitizeConf));
+        requestAnimationFrame(() => {
+            setSelectionRange(evt.currentTarget, prevSelection.start, prevSelection.end);
+        });
+    }, [setContent]);
 
     const onKeyDownHandler = (evt) => {
         if (evt.key === "Enter") {
@@ -54,8 +34,10 @@ export default function RenderedItem({id, type, content, setContent, onDelete, o
         id: id,
         html: content,
         className: styles.itemInner,
-        onChange: (evt) => setContent(evt.target.value),
-        onBlur: onContentChange,
+        onChange: (evt) => {
+            setContent(evt.target.value);
+            onContentChange(evt);
+        },
         onKeyDown: onKeyDownHandler,
     };
 
---
+++ src/components/workspace/selection/Selection.js
@@ -0,0 +1,139 @@
+function getTextLength(elem) {
+    let range = elem.ownerDocument.createRange();
+    range.selectNodeContents(elem);
+
+    return range.toString().length;
+}
+
+function getCaretOffset(elem) {
+    let sel = elem.ownerDocument.defaultView.getSelection();
+    if (sel.rangeCount === 0) return 0;
+
+    let range = elem.ownerDocument.defaultView.getSelection().getRangeAt(0);
+    let preCaretRange = range.cloneRange();
+    preCaretRange.selectNodeContents(elem);
+    preCaretRange.setEnd(range.endContainer, range.endOffset);
+    return preCaretRange.toString().length;
+}
+
+export function isCaretAtEnd(elem) {
+    if (elem.ownerDocument.activeElement !== elem) return false;
+    if (elem.ownerDocument.defaultView.getSelection().getRangeAt(0).toString().length > 0) return false;
+
+    return getCaretOffset(elem) === getTextLength(elem);
+}
+
+export function isCaretAtStart(elem) {
+    if (elem.ownerDocument.activeElement !== elem) return false;
+    if (elem.ownerDocument.defaultView.getSelection().getRangeAt(0).toString().length > 0) return false;
+
+    return getCaretOffset(elem) === 0;
+}
+
+export function getSelectionStart(contenteditable) {
+    const selection = window.getSelection();
+    const c = compareCaretPositions(selection.anchorNode, selection.anchorOffset, selection.focusNode, selection.focusOffset);
+    return c < 0 ? getTextOffset(contenteditable, selection.anchorNode, selection.anchorOffset) : getTextOffset(contenteditable, selection.focusNode, selection.focusOffset);
+}
+
+export function getSelectionEnd(contenteditable) {
+    const selection = window.getSelection();
+    const c = compareCaretPositions(selection.anchorNode, selection.anchorOffset, selection.focusNode, selection.focusOffset);
+    return c < 0 ? getTextOffset(contenteditable, selection.focusNode, selection.focusOffset) : getTextOffset(contenteditable, selection.anchorNode, selection.anchorOffset);
+}
+
+export function setSelectionRange(contenteditable, start, end) {
+    const selection = window.getSelection();
+    const s = getCaretPosition(contenteditable, start);
+    const e = getCaretPosition(contenteditable, end);
+    selection.setBaseAndExtent(s.node, s.offset, e.node, e.offset);
+}
+
+function getCaretPosition(contenteditable, textPosition) {
+    let textOffset = 0;
+    let lastNode = null;
+    let lastOffset = 0;
+    for (const p of positions(contenteditable)) {
+        if (p.text.length > textPosition - textOffset) {
+            return {node: p.node, offset: p.node.nodeType === Node.TEXT_NODE ? textPosition - textOffset : p.offset};
+        }
+        textOffset += p.text.length;
+        lastNode = p.node;
+        lastOffset = p.node.nodeType === Node.TEXT_NODE ? p.text.length : p.offset;
+    }
+    return {node: lastNode, offset: lastOffset};
+}
+
+function getTextOffset(contenteditable, selectionNode, selectionOffset) {
+    let textOffset = 0;
+    for (const p of positions(contenteditable)) {
+        if (selectionNode.nodeType !== Node.TEXT_NODE && selectionNode === p.node && selectionOffset === p.offset) {
+            return textOffset;
+        }
+        if (selectionNode.nodeType === Node.TEXT_NODE && selectionNode === p.node) {
+            return textOffset + selectionOffset;
+        }
+        textOffset += p.text.length;
+    }
+    return compareCaretPositions(selectionNode, selectionOffset, contenteditable, 0) < 0 ? 0 : textOffset;
+}
+
+function* positions(node, isLineStart = true) {
+    console.assert(node.nodeType === Node.ELEMENT_NODE);
+    let child = node.firstChild;
+    let offset = 0;
+    yield {node: node, offset: offset, text: stringifyElementStart(node, isLineStart)};
+    while (child != null) {
+        if (child.nodeType === Node.TEXT_NODE) {
+            yield {node: child, offset: 0 / 0, text: child.data};
+            isLineStart = false;
+        } else {
+            isLineStart = yield* positions(child, isLineStart);
+        }
+        child = child.nextSibling;
+        offset += 1;
+        yield {node: node, offset: offset, text: ''};
+    }
+    return isLineStart;
+}
+
+function stringifyElementStart(node, isLineStart) {
+    // if (node.tagName.toLowerCase() === 'br') {
+    //     return '\n';
+    // }
+    if (node.tagName.toLowerCase() === 'div') {
+        if (!isLineStart) {
+            return '\n';
+        }
+    }
+    return '';
+}
+
+function compareCaretPositions(node1, offset1, node2, offset2) {
+    if (node1 === node2) {
+        return offset1 - offset2;
+    }
+    const c = node1.compareDocumentPosition(node2);
+    if ((c & Node.DOCUMENT_POSITION_CONTAINED_BY) !== 0) {
+        return isAfter(node1, offset1, node2) ? +1 : -1;
+    } else if ((c & Node.DOCUMENT_POSITION_CONTAINS) !== 0) {
+        return isAfter(node2, offset2, node1) ? -1 : +1;
+    } else if ((c & Node.DOCUMENT_POSITION_FOLLOWING) !== 0) {
+        return -1;
+    } else if ((c & Node.DOCUMENT_POSITION_PRECEDING) !== 0) {
+        return +1;
+    }
+}
+
+function isAfter(container, offset, node) {
+    let c = node;
+    while (c.parentNode !== container) {
+        c = c.parentNode;
+    }
+    let i = offset;
+    while (c != null && i > 0) {
+        c = c.previousSibling;
+        i -= 1;
+    }
+    return i > 0;
+}
